bdef8bf3e9cc8ac1de803afc997e8bbb0498a917
Satyam750 <satyamthakur05@gmail.com>
2023-10-16 17:48:33 +0530

    first commit

---
+++ src/components/Price.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+import PriceCard from './PriceCard'
+import AnimatedShapes from './AnimatedShapes'
+
+const Container = styled.div`
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+@media only screen and (max-width: 480px) {
+  flex-direction: column;
+  }
+`
+
+const Price = () => {
+  return (
+    <Container>
+      <PriceCard price="10" type="Basic" />
+      <PriceCard price="20" type="Premium" />
+      <PriceCard price="30" type="Advanced" />
+      <AnimatedShapes/>
+    </Container>
+  )
+}
+
+export default Price
